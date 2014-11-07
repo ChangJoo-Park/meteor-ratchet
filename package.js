292b70e5c7d03f9e9ef29575fae17930f5b0e8a5
ParkChang Joo <ChangJooPark@ParkChangui-MacBook-Pro.local>
2014-11-08 01:37:26 +0900

    add css

--- package.js
+++ package.js
@@ -1,17 +1,29 @@
 Package.describe({
   name: 'pcjpcj2:ratchet',
   summary: ' Ratchet UI Framework for Meteor',
-  version: '1.0.0',
+  version: '2.0.2',
   git: 'https://github.com/pcjpcj2/meteor-ratchet'
 });
 
-Package.onUse(function(api) {
-  api.versionsFrom('1.0');
-  api.addFiles('pcjpcj2:ratchet.js');
-});
+Package.onUse(function (api) {
+  api.versionsFrom("0.9.3.1");
 
-Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('pcjpcj2:ratchet');
-  api.addFiles('pcjpcj2:ratchet-tests.js');
+  api.use('jquery');
+
+  var path = Npm.require('path');
+  var asset_path = path.join('ratchet-2.0.2');
+  api.addFiles(path.join(asset_path, 'css', 'ratchet.css'), 'client');
+  api.addFiles(path.join(asset_path, 'css', 'ratchet-theme-ios.css'), 'client');
+  api.addFiles(path.join(asset_path, 'js', 'ratchet.js'), 'client');
+
+  // fonts
+  api.addFiles(path.join(asset_path, 'fonts', 'ratchicons.eot'), 'client');
+  api.addFiles(path.join(asset_path, 'fonts', 'ratchicons.ttf'), 'client');
+  api.addFiles(path.join(asset_path, 'fonts', 'ratchicons.svg'), 'client');
+  api.addFiles(path.join(asset_path, 'fonts', 'ratchicons.woff'), 'client');
+
+  // XXX this makes the paths to the icon sets absolute. it needs
+  // to be included _after_ the standard ratchet css so
+  // that its styles take precedence.
+  api.addFiles(path.join('ratchet-override.css'), 'client');
 });
